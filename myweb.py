9bbdd6b7dd16a5827780e6448a6d4d3a2734d66b
Fong Jun Yuan <junyuanfong2000@gmail.com>
2021-01-16 23:30:16 +0800

    log in using the database info

--- myweb.py
+++ myweb.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request, render_template, flash
+from flask import Flask, redirect, url_for, request, render_template, session
 import sqlite3 as lit
 
 app = Flask(__name__)
@@ -11,32 +11,46 @@ def mainpage():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    user = request.form['name']
-    password = request.form['password']
-    print(user + password)
-    print("Hello world")
 
-    if user == "admin" and password == "admin":
-        return redirect(url_for('mainpage'))
-    else:
-        return render_template("login.html")
+    r=""
+    msg=" "
+    if(request.method == "POST"):
+        username = request.form['name']
+        password = request.form['password']
+        print(username + password)
+        connection = lit.connect("userdata.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE username= '"+username+"' and password ='"+password+"' ")
+        r = cursor.fetchall()
+        for i in r:
+            if(username == i[0] and password == i[1]):
+                return redirect(url_for('mainpage'))
+        else:
+            msg = "Please enter valid username and password"
+    return render_template('login.html',msg=msg)
+
+
 
 
 @app.route('/user/signup', methods=['POST'])
 def signup():
-    runame = request.form['unm']
-    rpassword = request.form['psd']
+    username = request.form['unm']
+    password = request.form['psd']
     rname = request.form['nm']
     email = request.form['email']
 
-    print(runame + "" + rpassword + "" + rname + "" + email)
+    print(username + "" + password + "" + rname + "" + email)
     connection = lit.connect("userdata.db")
     cursor = connection.cursor()
-    query1 = "INSERT INTO users VALUES('{un}','{pd}','{n}','{em}')".format(un=runame, pd=rpassword, n=rname, em=email)
+    query1 = "INSERT INTO users VALUES('{un}','{pd}','{n}','{em}')".format(un=username, pd=password, n=rname, em=email)
     cursor.execute(query1)
     connection.commit()
     return render_template("login.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
